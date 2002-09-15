76f4612c3f9bf08a0b314b7b18fe08bdbcd15ab8
johnny <johnny>
2002-09-15 20:02:09 +0000

    docs/COPYING.txt: 	#  centralized docs under /docs 	#  merged CREDITS.txt files 	# 	#  moved obsolete db diagrams to /test docs/COPYING.txt: 	#  RCS to BitKeeper docs/pn.dsl: 	#  Document sources for the MDG and API docs/pn.dsl: 	#  RCS to BitKeeper docs/pdfize.sh: 	#  Jim's pdfize script. docs/pdfize.sh: 	#  RCS to BitKeeper docs/pnMDG.dbk: 	#  added to do docs/pnMDG.dbk: 	#  removed templating to be done. docs/pnMDG.dbk: 	#  Exceptions! docs/pnMDG.dbk: 	#  removed spurious tag docs/pnMDG.dbk: 	#  speling updates docs/pnMDG.dbk: 	#  need to use character entity for less than symbol docs/pnMDG.dbk: 	#  Prepared for release. docs/pnMDG.dbk: 	#  Better documentation for User Variables and Variable Validation sections. docs/pnMDG.dbk: 	#  added section on variable validation docs/pnMDG.dbk: 	#  added more user vars todos docs/pnMDG.dbk: 	#  added changelog, user variables, pn architecture chapter docs/pnMDG.dbk: 	#  add error handling todo docs/pnMDG.dbk: 	#  consistency updates: lower-case module names 	#  added notes about missing parts 	#  some spelling mistakes corrected docs/pnMDG.dbk: 	#  updated DOCTYPE docs/pnMDG.dbk: 	#  Document sources for the MDG and API docs/pnMDG.dbk: 	#  RCS to BitKeeper docs/PN_QUALITY.txt: 	#  removed persona non grata. docs/PN_QUALITY.txt: 	#  centralized docs under /docs 	#  merged CREDITS.txt files 	# 	#  moved obsolete db diagrams to /test docs/PN_QUALITY.txt: 	#  RCS to BitKeeper docs/pnAPI.dbk: 	#  re-add pnServerGetVar and pnVar*Cached functions (from before mako's core changes) docs/pnAPI.dbk: 	#  Exceptions! docs/pnAPI.dbk: 	#  Prepared for release. docs/pnAPI.dbk: 	#  Documented pnUserValidateVar. 	#  Modified pnUserSetVar and pnUserGetVar to reflect API changes. docs/pnAPI.dbk: 	#  Deprecated pnUserGetVars. docs/pnAPI.dbk: 	#  Deprecated pnUserGetAll docs/pnAPI.dbk: 	#  updated DOCTYPE and character entites docs/pnAPI.dbk: 	#  fixes for xml parsing.. duh! :) docs/pnAPI.dbk: 	#  many functions added, expanded authorship docs/pnAPI.dbk: 	#  pnUserGetCommentsArray removed. 	# 	#  this function should never have made it to the API, it was broken from the start. docs/pnAPI.dbk: 	#  Document sources for the MDG and API docs/pnAPI.dbk: 	#  RCS to BitKeeper docs/CREDITS.txt: 	#  added spliffster to the credits docs/CREDITS.txt: 	#  Added credits to Tom O docs/CREDITS.txt: 	#  test commit :) hello syncmail? docs/CREDITS.txt: 	#  sysinfo screen added. contains php version, loaded modules etc. needs to be fleshed out docs/CREDITS.txt: 	#  Changed my own info. First test commit on new repository. docs/CREDITS.txt: 	#  Added me docs/CREDITS.txt: 	#  changed email address for credit problems docs/CREDITS.txt: 	#  centralized docs under /docs 	#  merged CREDITS.txt files 	# 	#  moved obsolete db diagrams to /test docs/CREDITS.txt: 	#  RCS to BitKeeper

---
+++ docs/pdfize.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+openjade -t tex -d pn.dsl $1.dbk && {
+        # Fix bad _ in template headers
+        mv $1.tex $1.otex
+        sed 's/_/\\_/g' <$1.otex >$1.tex
+        rm -f $1.otex
+        pdfjadetex $1.tex
+        pdfjadetex $1.tex
+        pdfjadetex $1.tex
+}

0bf006dec6a1ccb0be3467a0cbe08fef744f1bd2
miko <miko@miko.homelinux.org>
2002-11-29 20:02:11 +0000

    docs/dev/pdfize.sh: 	#  prefix changes

--- docs/dev/pdfize.sh
+++ docs/dev/pdfize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-openjade -t tex -d pn.dsl $1.dbk && {
+openjade -t tex -d xar.dsl $1.dbk && {
         # Fix bad _ in template headers
         mv $1.tex $1.otex
         sed 's/_/\\_/g' <$1.otex >$1.tex

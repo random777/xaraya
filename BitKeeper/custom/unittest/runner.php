9dc218f253c98aae554662b52ea8f0703645496f
marcel <marcel@hsdev.com>
2003-03-17 20:03:03 +0000

    BitKeeper/custom/unittest/runner.php: 	#  A BitKeeper/custom/unittest/xartests/testUnitTest.php: 	#  BitKeeper file /var/www/html/xaraya/BitKeeper/custom/unittest/xartests/testUnitTest.php BitKeeper/custom/unittest/runner.php: 	#  headers and stuff BitKeeper/custom/unittest/runner.php: 	#  headers and stuff BitKeeper/custom/unittest/runner.php: 	#  merged jan's changes in here BitKeeper/custom/unittest/runner.php: 	#  headers and stuff

--- BitKeeper/custom/unittest/runner.php
+++ BitKeeper/custom/unittest/runner.php
@@ -1,8 +1,33 @@
 <?php
+
+/**
+ * File: $Id$
+ *
+ * Runner for the unit testing framework
+ *
+ * @package quality
+ * @copyright (C) 2002 by the Xaraya Development Team.
+ * @link http://www.xaraya.com
+ * 
+ * @subpackage testing
+ * @author Marcel van der Boom <marcel@xaraya.com>
+ * @author Jan Schrage <jan@xaraya.com>
+ * @author Frank Besler <besfred@xaraya.com>
+*/
+
 echo "Starting tests...\n";
 
+/**
+ * Include the framework
+ *
+ */
 include_once "./xarUnitTest.php";
 
+/** 
+ * Define the array which holds the testsuites
+ *
+ * Define a default suite which normally holds tests which are not in another testsuite
+ */
 $suites= array();
 $suites[] = new xarTestSuite();
 $suite=&$suites[0];
@@ -14,11 +39,11 @@ include_once "./xartests/testUnitTest.php";
 
 include_once "../../../html/includes/xartests/testBLCompiler.php";
 chdir("../../../html/includes/xartests");
+
 // Cycle through all suites
 foreach ($suites as $torun) {
     $torun->run();
     $torun->report('text');
 }
-
 
 ?>

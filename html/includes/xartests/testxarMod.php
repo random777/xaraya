91d9b2e9d1e08f92adea7a27c47a7ae46deebe5b
John.Cox <John.Cox@mcnabb.>
2003-03-23 20:03:03 +0000

    html/includes/xartests/testxarVar.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya/html/includes/xartests/testxarVar.php BitKeeper/custom/unittest/runner.php: 	#  deleted empty line after ?> BitKeeper/custom/unittest/xarUnitTest.php: 	#  hardened the asserts html/includes/xartests/testBLCompiler.php: 	#  deleted empty line after ?> 	#  added global var in setup html/includes/xartests/testxarUser.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya/html/includes/xartests/testxarUser.php html/includes/xartests/testxarUser.php: 	#  unit testing html/includes/xartests/testxarMod.php: 	#  BitKeeper file C:/cygwin/home/phpdev/www/dev/xaraya/html/includes/xartests/testxarMod.php

---
+++ html/includes/xartests/testxarMod.php
@@ -0,0 +1,44 @@
+<?php
+
+class testxarMod extends xarTestCase {
+    var $myBLC;
+    
+    function setup() {
+    include_once 'xarCore.php';
+    include_once 'xarLog.php';
+    include_once 'xarDB.php';
+    include_once 'xarServer.php';
+    include_once 'xarMod.php';
+    }
+    
+    function precondition() {
+        return true;
+    }
+
+    function teardown () {
+        // not needed here
+    }
+
+    function testInit() {
+        return $this->assertTrue(xarMod_init('',''),"Module System Initialisation");
+    }
+    
+    /* Doesn't work.
+    function testLoadAPI() {
+        return $this->assertTrue(xarModAPIFunc($modName = 'base', $modType = 'user', $funcName = 'handleBaseURLTag'),"Load an API File");
+    }
+    */
+    function testLoadAPI() {
+        return $this->assertTrue(xarMod_getFileInfo('base'),"Check File Info");
+    }
+
+    /*
+    function testLoadAPI() {
+        return $this->assertTrue(xarModIsAvailable('base'),"Check File Info");
+    }
+    */
+}
+
+$suite->AddTestCase('testxarMod','Testing xarMod.php');
+
+?>
--- html/includes/xartests/testxarUser.php
+++ html/includes/xartests/testxarUser.php
@@ -6,6 +6,9 @@ class testxarUser extends xarTestCase {
     function setup() {
         include_once 'xarCore.php';
         include_once 'xarVar.php';
+        include_once 'xarMLS.php';
+        include_once 'xarTemplate.php';
+        include_once 'xarLog.php';
         include_once 'xarUser.php';
         include_once 'xarSession.php';
         include_once 'xarServer.php';
@@ -25,6 +28,11 @@ class testxarUser extends xarTestCase {
         $username = 'Admin';
         $password = 'password';
         return $this->assertTrue(xarUserLogIn($username, $password, $rememberme = 0),"Check a user logging in");
+    }
+    */
+    /*
+    function testInit() {
+        return $this->assertTrue(xarUser_init('',''),"User System Initialisation");
     }
     */
     function testUserVar() {

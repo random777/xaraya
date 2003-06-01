61ae193f0a9d5f73232e84ade054538ed2e3478c
marcel <marcel@hsdev.com>
2003-06-01 20:03:06 +0000

    html/includes/xartests/testMLSystem.php: 	#  BitKeeper file /var/www/xar/stable/main/html/includes/xartests/testMLSystem.php

---
+++ html/includes/xartests/testMLSystem.php
@@ -0,0 +1,22 @@
+<?php
+
+class testMLSSystem extends xarTestCase {
+
+    function setup() {
+        include_once  'xarMLS.php';
+    }
+
+
+    function testEmptyMLString() {
+        return true;
+    }
+
+
+
+
+
+}
+$suite->AddTestCase('testMLSSystem','MLS system tests');
+
+
+?>

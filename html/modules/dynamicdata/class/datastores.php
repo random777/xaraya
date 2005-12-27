22557ff84ed37974fdbd4194417b846f6940ec16
johnny <johnny@localmomentum.net>
2005-12-27 11:08:18 +0000

    adding some phpdoc 	* html/modules/dynamicdata/class/datastores.php: ... Datastores development branch: this branch is used to extend datastores Changes to implement: 1. implement the ability to add/delete/modify instances of datastores 2. clean up the datastore property by adding the ability to choose an instance of a datastore 3. add extra configuration variables per datastore and per instance 4. implement the filebased datastores (xml, csv)

--- html/modules/dynamicdata/class/datastores.php
+++ html/modules/dynamicdata/class/datastores.php
@@ -2,25 +2,27 @@
 /**
  * Utility Class to manage Dynamic Data Stores
  *
- * @package Xaraya eXtensible Management System
+ * @package modules
  * @copyright (C) 2005 The Digital Development Foundation
  * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
  * @link http://www.xaraya.com
  *
- * @subpackage Dynamicdata module
+ * @subpackage dynamicdata
  * @author mikespub <mikespub@xaraya.com>
  */
 
 /**
  * Utility Class to manage Dynamic Data Stores
- *
- * @package Xaraya eXtensible Management System
- * @subpackage dynamicdata module
+ * @author mikespub <mikespub@xaraya.com>
  */
 class Dynamic_DataStore_Master
 {
     /**
      * Class method to get a new dynamic data store (of the right type)
+     *
+     * @param string $name
+     * @param string $type
+     * @return object
      */
     function &getDataStore($name = '_dynamic_data_', $type = 'data')
     {
@@ -82,7 +84,8 @@ class Dynamic_DataStore_Master
     /**
      * Get possible data sources (// TODO: for a module ?)
      *
-     * @param $args['table'] optional extra table whose fields you want to add as potential data source
+     * @param array $args optional extra table whose fields you want to add as potential data source
+     * @return array
      */
     function &getDataSources($args = array())
     {
@@ -100,7 +103,7 @@ class Dynamic_DataStore_Master
         // session variables // TODO: perhaps someday, if this makes sense
         //$sources[] = 'session variables';
 
-    // TODO: re-evaluate this once we're further along
+        // TODO: re-evaluate this once we're further along
         // hook modules manage their own data
         $sources[] = 'hook module';
 
@@ -128,7 +131,7 @@ class Dynamic_DataStore_Master
         $systemPrefix = xarDBGetSystemTablePrefix();
         $metaTable = $systemPrefix . '_tables';
 
-    // TODO: remove Xaraya system tables from the list of available sources ?
+        // TODO: remove Xaraya system tables from the list of available sources ?
         $query = "SELECT xar_table,
                          xar_field,
                          xar_type,
@@ -143,7 +146,7 @@ class Dynamic_DataStore_Master
         // add the list of table + field
         while (!$result->EOF) {
             list($table, $field, $type, $size) = $result->fields;
-        // TODO: what kind of security checks do we want/need here ?
+            // TODO: what kind of security checks do we want/need here ?
             //if (xarSecAuthAction(0, 'DynamicData::Field', "$name:$type:$id", ACCESS_READ)) {
             //}
             $sources[] = "$table.$field";
@@ -159,14 +162,22 @@ class Dynamic_DataStore_Master
 /**
  * Base class for Dynamic Data Stores
  *
- * @package Xaraya eXtensible Management System
- * @subpackage dynamicdata module
+ * @var string $name some static name, or the table name, or the moduleid + itemtype, or ...
+ * @var string $type
+ * @var array  $fields array of $name => reference to property in Dynamic_Object*
+ * @var string $primary
+ * @var array  $sort
+ * @var array  $where
+ * @var array  $groupby
+ * @var array  $join
+ * @var array  $_itemids reference to itemids in Dynamic_Object_List
+ * @var int    $cache
  */
 class Dynamic_DataStore
 {
-    var $name;     // some static name, or the table name, or the moduleid + itemtype, or ...
+    var $name;
     var $type;
-    var $fields;   // array of $name => reference to property in Dynamic_Object*
+    var $fields;
     var $primary;
 
     var $sort;
@@ -174,10 +185,15 @@ class Dynamic_DataStore
     var $groupby;
     var $join;
 
-    var $_itemids;  // reference to itemids in Dynamic_Object_List
+    var $_itemids;
 
     var $cache = 0;
 
+    /**
+     * Constructor
+     * 
+     * @param string name
+     */
     function Dynamic_DataStore($name)
     {
         $this->name = $name;
@@ -191,6 +207,9 @@ class Dynamic_DataStore
 
     /**
      * Get the field name used to identify this property (by default, the property name itself)
+     *
+     * @param object $property
+     * @return string
      */
     function getFieldName(&$property)
     {
@@ -199,6 +218,8 @@ class Dynamic_DataStore
 
     /**
      * Add a field to get/set in this data store, and its corresponding property
+     *
+     * @param object $property
      */
     function addField(&$property)
     {
@@ -214,6 +235,8 @@ class Dynamic_DataStore
 
     /**
      * Set the primary key for this data store (only 1 allowed for now)
+     *
+     * @param object $property
      */
     function setPrimary(&$property)
     {
@@ -223,30 +246,65 @@ class Dynamic_DataStore
         $this->primary = $name;
     }
 
+    /**
+     * Get item
+     *
+     * @param array $args
+     * @return int
+     */
     function getItem($args)
     {
         return $args['itemid'];
     }
-
+    
+    /**
+     * Create item
+     *
+     * @param array $args
+     * @return int
+     */
     function createItem($args)
     {
         return $args['itemid'];
     }
-
+    
+    /**
+     * Update item
+     *
+     * @param array $args
+     * @return int
+     */
     function updateItem($args)
     {
         return $args['itemid'];
     }
 
+    /**
+     * Delete item
+     *
+     * @param array $args
+     * @return int
+     */
     function deleteItem($args)
     {
         return $args['itemid'];
     }
 
+    /**
+     * Get items
+     *
+     * @param array $args
+     */
     function getItems($args = array())
     {
     }
-
+    
+    /**
+     * Count items
+     *
+     * @param array $args
+     * @return null
+     */
     function countItems($args = array())
     {
         return null;
@@ -254,6 +312,9 @@ class Dynamic_DataStore
 
     /**
      * Add a sort criteria for this data store (for getItems)
+     *
+     * @param object property
+     * @param string $sortorder (ASC or DESC)
      */
     function addSort(&$property, $sortorder = 'ASC')
     {
@@ -274,6 +335,12 @@ class Dynamic_DataStore
 
     /**
      * Add a where clause for this data store (for getItems)
+     *
+     * @param object $property
+     * @param $clause
+     * @param array  $join
+     * @param string $pre
+     * @param string $post
      */
     function addWhere(&$property, $clause, $join, $pre = '', $post = '')
     {
@@ -297,6 +364,8 @@ class Dynamic_DataStore
 
     /**
      * Add a group by field for this data store (for getItems)
+     *
+     * @param object $property
      */
     function addGroupBy(&$property)
     {
@@ -315,7 +384,16 @@ class Dynamic_DataStore
     }
 
     /**
-     * Join another database table to this data store (unfinished)
+     * Join another database table to this data store
+     *
+     * @param string $table
+     * @param string $key
+     * @param array  $fields
+     * @param string $andor
+     * @param string $more
+     * @param array  $sort
+     *
+     * @todo finish
      */
     function addJoin($table, $key, $fields, $where = array(), $andor = 'and', $more = '', $sort = array())
     {
@@ -333,7 +411,7 @@ class Dynamic_DataStore
         $whereclause = '';
         if (is_array($where) && count($where) > 0) {
             foreach ($where as $part) {
-// TODO: support pre- and post-parts here too ? (cfr. bug 3090)
+                // TODO: support pre- and post-parts here too ? (cfr. bug 3090)
                 $whereclause .= $part['join'] . ' ' . $part['property']->source . ' ' . $part['clause'] . ' ';
             }
         } elseif (is_string($where)) {
@@ -354,7 +432,5 @@ class Dynamic_DataStore
     {
         $this->join = array();
     }
-
 }
-
 ?>

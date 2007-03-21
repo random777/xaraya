5008d34da536d498f06315077dbe0cc05ef7bff8
mfl <mfl@netspan.ch>
2007-03-21 18:51:04 +0000

    quick fix for bug 5959

--- html/modules/base/xarproperties/Dynamic_URL_Property.php
+++ html/modules/base/xarproperties/Dynamic_URL_Property.php
@@ -35,22 +35,37 @@ class Dynamic_URL_Property extends Dynamic_TextBox_Property
         if (!isset($value)) {
             $value = $this->value;
         }
+        // Make sure $value['link'] is set, has a length > 0 and does not equal simply 'http://'
         $value = trim($value);
-        if (!empty($value) && $value != 'http://') {
-        // TODO: add some URL validation routine !
-        // see note under http://bugs.xaraya.com/show_bug.cgi?id=5959
+        if (!empty($value) && $value != 'http://')  {
+           //let's process futher then
+           //check it is not invalid eg html tag
             if (preg_match('/[<>"]/',$value)) {
                 $this->invalid = xarML('URL');
-                $this->value = null;
+                $this->value = '';
                 return false;
-            } else if (substr($value, 0, 4) == 'www.') {
-                // allow users to say www.mysite.com
-                $this->value = 'http://' . $value;
             } else {
-                $this->value = $value;
-            }
-        } else {
-            $this->value = '';
+              // If we have a scheme but nothing following it,
+                // then consider the link empty :-)
+                if (eregi('^[a-z]+\:\/\/$', $value)) {
+                    $this->value = '';
+                } else {
+                    // Do some URL validation below. Separate for better understanding
+                    // Still not perfect. Add as seen fit.
+                    $uri = parse_url($value);
+                    if (empty($uri['scheme']) && empty($uri['host']) && empty($uri['path'])) {
+                        $this->invalid = xarML('URL');
+                        $this->value = '';
+                        return false;
+                    } elseif (empty($uri['scheme'])) {
+                        $this->value = 'http://' . $value;
+                    } else {
+                        // it has at least a scheme (http/ftp/etc) and a host (domain.tld)
+                        $this->value = $value;
+                    }
+                }
+
+            } //end checks for other schemes
         }
         return true;
     }

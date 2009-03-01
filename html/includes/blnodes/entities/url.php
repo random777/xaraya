fb13f304d7968e3e8691b8c7df6bf81a33c92c6c
hb <hb@xaraya.com>
2009-03-01 16:02:46 +0000

    BlockLayout: For Entity &xar-url; all parameters are optional now. xarModURL() parameters 'args', 'fragment' and 'generateXMLURL' are part of the entity now. 'args' can be given as string 'argument'=>'value' or as name of an array with these pairs.. The entity uses INVALID characters. See bug 6341. Allowed are only _ [a-zA-Z] [0-9] : . -

--- html/includes/blnodes/entities/url.php
+++ html/includes/blnodes/entities/url.php
@@ -1,10 +1,14 @@
 <?php
 
 /**
-* xarTpl_XarUrlEntityNode
+ * xarTpl_XarUrlEntityNode
  *
- * More generic than ModUrlEntityNode, supports args
- * this wraps xarModURL('$module', '$type', '$func'$args)
+ * More generic than ModUrlEntityNode, supports args, fragments and generateXMLURL
+ * The 'hasExtras' workaround for bug 3603 is deprecated for this entity &xar-url;
+ *
+ * @copyright (C) 2002-2009 The Digital Development Foundation
+ * @license GPL {@link http://www.gnu.org/licenses/gpl.html}
+ * @link http://www.xaraya.com
  *
  * @package blocklayout
  * @access private
@@ -12,38 +16,71 @@
  */
 class xarTpl__XarUrlEntityNode extends xarTpl__EntityNode
 {
+    /**
+     * Renders the entitiy &xar-url-module-type-func-argsArray-fragment-generateXMLURL
+     *
+     * This entity wraps xarModURL(). As in the function all parameters are optional.
+     * See the function for details, 'fragment' and 'generateXMLURL' changed their positions
+     * Use 'Null' for unneeded parameters to skip them
+     * argsArray can be:
+     * * $name of an array with parameter=>value pairs OR
+     * * a list of 'parameter'=>'value' pairs as arguments for an array() function OR
+     * * 'Null' to skip
+     */
     function render()
     {
-        if (count($this->parameters) < 3) {
+        if (count($this->parameters) > 7) {
             $this->raiseError(XAR_BL_MISSING_PARAMETER,'Parameters mismatch in &xar-url entity.', $this);
             return;
         }
-        $module = $this->parameters[0];
-        if ($module == '') {
-            $tplVars =& xarTpl__TemplateVariables::instance();
-            $module = $tplVars->get('module');
-            if (empty($module)) {
-                $this->raiseError(XAR_BL_MISSING_PARAMETER,'Empty module parameter in &xar-url entity.', $this);
-                return;
+
+        // Build the arguments for xarModURL beginning from the last parameter.
+        // Parameters which position differs between entity and function need to
+        // be preset with their default value and placed afterwards
+        $args ='';
+        $generateXMLURL = Null;
+
+        switch (count($this->parameters)) {
+        case 7 :
+            // TODO entrypoint
+        case 6 :
+            //XMLURLS
+            if (strtolower($this->parameters[5]) != 'null') {
+                $generateXMLURL = $this->parameters[5];
             }
+        case 5 :
+            // fragment, anchor
+            if (strtolower($this->parameters[4]) == 'null') {
+                $args = "', Null'" . $args;
+            } else {
+                $args = ", '".$this->parameters[4]."'" . $args;
+            }
+            if (empty($generateXMLURL)) {
+                $args = ", Null" . $args;
+            } else {
+                $args = ", '".$generateXMLURL."'" . $args;
+            }
+        case 4 :
+            // args
+            if (strtolower($this->parameters[3]) == 'null') {
+                $args = ", array()" . $args;
+            } elseif (substr($this->parameters[3], 0, 1) == '$') {
+                $args = ', '.$this->parameters[3] . $args;
+            } else {
+                $args = ", array(".$this->parameters[3].")" . $args;
+            }
+        case 3 :
+            // function
+            $args = ", '".$this->parameters[2]."'" . $args;
+        case 2 :
+            // type
+            $args = ", '".$this->parameters[1]."'" . $args;
+        case 1 :
+            // module
+            $args = "'".$this->parameters[0]."'" . $args;
         }
-        $type = $this->parameters[1];
-        if ($type == '') {
-            $type = 'user';
-        }
-        $func = $this->parameters[2];
-        if ($func == '') {
-            $func = 'main';
-        }
-        $args = '';
-        if (isset($this->parameters[3])) {
-            $args = ', array($'.$this->parameters[3].')';
-        } elseif($this->hasExtras) {
-            // If the template specifies extra params with &amp;, notify xarModUrl of this,
-            // so it can generate the proper ?. Workaround for bug 3603
-            $args = ", array(NULL=>NULL)";
-        }
-        return "xarModURL('$module', '$type', '$func'$args)";
+
+        return "xarModURL($args)";
     }
 }
 ?>

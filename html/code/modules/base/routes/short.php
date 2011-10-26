1bb58cbd3242563c90e805e9dae9bd83b2043b88
crispcreations <crispcreations@googlemail.com>
2011-10-26 17:16:19 +0000

    Add base module short route handler

---
+++ html/code/modules/base/routes/short.php
@@ -0,0 +1,35 @@
+<?php
+sys::import('xaraya.mapper2.routes.base');
+class BaseShortRoute extends BaseRoute 
+{
+    public function encode(ixarUrl $url)
+    {
+        // when encoding, mod, type, func and args will be available from $url
+        if ($url->getType() == 'admin') return;
+        
+        $path = $url->getPath();        
+        $query = $url->getArgs();
+        if (!empty($query['page'])) {
+            $path[] = $query['page'];
+            unset($query['page']);
+        }
+        
+        $url->setPath($path);
+        $url->setQuery($query);
+        return $url;                
+    }
+    
+    public function decode(ixarUrl $url)
+    {
+         if ($url->getType() == 'admin') return;
+         
+         $path = $url->getPath();
+         $args = $url->getQuery();
+         if (!empty($path[1]))
+             $args['page'] = $path[1];
+         
+         $url->setArgs($args);
+         return $url;   
+    }
+}
+?>

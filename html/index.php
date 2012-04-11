d9d35ee02ac3c41110d42b29caa82e6168dd324d
mfl <mfl@netspan.ch>
2012-04-11 11:18:46 +0000

    cosmetics

--- html/index.php
+++ html/index.php
@@ -62,7 +62,7 @@ function xarMain()
     $request = xarController::getRequest();
     xarController::normalizeRequest();
 
-    // Theme Override
+    // Set the theme. This happens early, because the choice of theme may influence the code
     xarVarFetch('theme','str:1:',$theme,'',XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY);
     xarDevice::configTheme($theme);
 
@@ -107,18 +107,11 @@ function xarMain()
         }
 
         // We're all done, one ServerRequest made
-        //xarEvents::trigger('ServerRequest');
         xarEvents::notify('ServerRequest');
         
-        // Set page template
-        $device = xarDevice::getRequestingDevice();
-        
-        xarDevice::configPageTemplate();
-
+        // Set page template. This happens after the code is done   
         xarVarFetch('pageName','str:1:', $pageName, '', XARVAR_NOT_REQUIRED, XARVAR_PREP_FOR_DISPLAY);
-        if (!empty($pageName)){
-            xarTpl::setPageTemplateName($pageName);
-        }
+        xarDevice::configPageTemplate($pageName);
 
         // Render page with the output
         $pageOutput = xarTpl::renderPage($mainModuleOutput);
--- html/lib/xaraya/device.php
+++ html/lib/xaraya/device.php
@@ -85,31 +85,36 @@ class xarDevice extends Object implements ixarDevice
     {
         $set = false;
         if (xarTpl::getPageTemplateName() != 'default') return $set;
-        
         $set = true;
-        $request = xarController::getRequest();
-        if (!xarUserIsLoggedIn() && $request->getType() == 'user') {
-            // For the anonymous user, see if a module specific page exists
-            if (!xarTplSetPageTemplateName('user-'.$request->getModule())) {
-                xarTplSetPageTemplateName($request->getModule());
-            }
-            return $set;
-        }
-
-        if (xarUserIsLoggedIn()) {
-            if (xarUserIsLoggedIn() && $request->getType() == 'user') {
-                // Same thing for user side where user is logged in
-                if (!xarTpl::setPageTemplateName('user-'.$request->getModule())) {
-                    xarTpl::setPageTemplateName('user');
+        
+        if (empty($template)) {
+            $request = xarController::getRequest();
+            if (!xarUserIsLoggedIn() && $request->getType() == 'user') {
+                // For the anonymous user, see if a module specific page exists
+                if (!xarTplSetPageTemplateName('user-'.$request->getModule())) {
+                    xarTplSetPageTemplateName($request->getModule());
                 }
-            } elseif (xarUserIsLoggedIn() && $request->getType() == 'admin') {
-                 // Use the admin-$modName.xt page if available when $modType is admin
-                // falling back on admin.xt if the former isn't available
-                if (!xarTpl::setPageTemplateName('admin-'.$request->getModule())) {
-                    xarTpl::setPageTemplateName('admin');
+                return $set;
+            }
+    
+            if (xarUserIsLoggedIn()) {
+                if (xarUserIsLoggedIn() && $request->getType() == 'user') {
+                    // Same thing for user side where user is logged in
+                    if (!xarTpl::setPageTemplateName('user-'.$request->getModule())) {
+                        xarTpl::setPageTemplateName('user');
+                    }
+                } elseif (xarUserIsLoggedIn() && $request->getType() == 'admin') {
+                     // Use the admin-$modName.xt page if available when $modType is admin
+                    // falling back on admin.xt if the former isn't available
+                    if (!xarTpl::setPageTemplateName('admin-'.$request->getModule())) {
+                        xarTpl::setPageTemplateName('admin');
+                    }
                 }
             }
+        } else {
+            xarTpl::setPageTemplateName($pageName);
         }
+        return $set;
     }
 }
 
